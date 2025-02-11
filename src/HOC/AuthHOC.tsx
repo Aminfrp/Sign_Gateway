682347693ddf6a25213ce578a6efaaeda47d6ce3
Amin Firouzpour <amin.firouzpour.myself@gmail.com>
2025-02-11 10:23:18 +0330

    docs: :fire: remove useless commented codes

--- src/HOC/AuthHOC.tsx
+++ src/HOC/AuthHOC.tsx
@@ -1,22 +1,19 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
 
-// Function to check authentication based on localStorage token
 const checkAuth = (): boolean => {
   return Boolean(localStorage.getItem("token"));
 };
 
 function withAuth<T extends object>(WrappedComponent: React.ComponentType<T>) {
   return (props: T) => {
-    const navigate = useNavigate(); // Initialize useNavigate hook
-
+    const navigate = useNavigate();
     useEffect(() => {
       if (!checkAuth()) {
         navigate("/auth/login", { replace: true });
       }
     }, []);
 
-    // If authenticated, render the wrapped component
     return <WrappedComponent {...props} />;
   };
 }
